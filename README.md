6c50101a7517ff845cc463dd98397c5a0f02ae35
velvetklr-iHGRgW <velvetklr@gmail.com>
2024-01-25 02:13:38 -0800

    uploading app.py with tensor mode and README

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
+![app_image](https://www.tetratech.com/wp-content/uploads/2023/10/Evaluating-the-Risk-of-Microplastics-in-Coastal-Waters-One-Water.jpg)
+
 # P4_MicroPystics
+
+## Description
  Using Machine Learning, Image Recognition, and Computer Vision to detect microplastics in water samples.
 
-**Original Data Source:**
-Microplastic Dataset for Computer Vision, MOHAMADREZA MOMENI
-https://www.kaggle.com/datasets/imtkaggleteam/microplastic-dataset-for-computer-vision
+ ## Table of Contents
+- [Installation](#installation)
+- [Data](#data)
+- [License](#license)
+- [Dashboard](#dashboard)
+- [Contact](#contact)
+
+## Installation
+To run the model in Google Colab we installed: 
+- tensorflow
+- drive
+- numpy
+- os
+- skimage.color
+- matplotlib  
+  
+For the Flask API to run the following was installed: 
+- os
+- flask
+- tensorflow
+- Pillow
+- numpy
+
+# Data
+
+## Source Data
+- #### [Microplastic Dataset for Computer Vision, MOHAMADREZA MOMENI:](https://www.kaggle.com/datasets/imtkaggleteam/microplastic-dataset-for-computer-vision)
+- #### Place holder for Aspen data
+
+# Results and evaluation
+
+
+## License
+MIT
+
+## Dashboard
+#### [Tableu Dashboard]
+
+## Contact
+If there are any questions or concerns, we can be reached at:
+##### [github: aspenjack](https://github.com/aspenjack)
+##### [github: arpitas0690](https://github.com/arpitas0690)
+##### [github: S-Bonillas](https://github.com/S-Bonillas)
+##### [github: Sequoiabm](https://github.com/Sequoiabm)
+##### [github: velvetklr](https://github.com/velvetklr)
+
+
--- app.py
+++ app.py
@@ -3,23 +3,61 @@
 # Import libraries
 import os
 from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
-import pickle
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import numpy as np
 
 # Create upload file path
 
 upload_folder = "static/upload"
+temp = "model/"
 
 # Create an app
 app = Flask(__name__)
 app.config['upload_folder'] = upload_folder
 
 # Load the model
-#model = pickle.load(open('./model.pkl','rb'))
+#model_path = os.path.abspath("model/microP.h5")
+#model = load_model(model_path)
+#model = load_model(model_path)
+model = tf.keras.models.load_model("model/saved_model")
+
+
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
+def preprocess_image(image_path):
+    # Read the image using OpenCV
+    image = Image.open(image_path).convert("RGB")
+
+    # print("Image Channels (PIL):", image.mode)
+    # print("Image Shape (PIL):", np.array(image).shape)
+
+    # Resize the image to (150, 150)
+    image = tf.image.resize(image, (128, 128))
+
+    # Convert the image to a NumPy array
+    image_array = np.array(image)
+
+    # Normalize the image
+    image_array = image_array / 255.0  # Assuming pixel values are in the range [0, 255]
+
+    image_array = np.transpose(image_array, (0, 1, 2))
+
+    # Add batch dimension
+    image_array = np.expand_dims(image_array, axis=0)
+
+    image_tensor = tf.convert_to_tensor(image_array, dtype=tf.float32)
+
+    return image_tensor
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
@@ -34,7 +72,16 @@ def predict():
 
         print("Path:",path)
 
-        return render_template("results.html", image_path=path)
+        input_data = preprocess_image(path)
+        predictions = model.predict(input_data)
+
+        binary_predictions = (predictions > 0.5).astype(int)
+
+        class_predict = "Clean" if binary_predictions == 0 else "Microplastics"
+
+        return render_template("results.html", image_path=path, class_predict=class_predict)
+    
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
